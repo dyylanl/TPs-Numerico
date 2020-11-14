4a906f132f5e78df28dcb3d2a77c411a1c9565e6
gcompanys <51275717+gcompanys@users.noreply.github.com>
2020-11-14 19:40:28 -0300

    .

--- TP 1/Metodos Busqueda de Raices/RaicesRealesDeCuadraticaConResolvente.py
+++ TP 1/Metodos Busqueda de Raices/RaicesRealesDeCuadraticaConResolvente.py
@@ -10,14 +10,14 @@ a = np.float32(input("a: "))
 b = np.float32(input('b: '))
 c = np.float32(input('c: '))
 
-discriminante = b**2 - 4*a*c
+discriminante = np.float32(b**2 - 4*a*c)
 
 if discriminante < 0:
     print('Las raices son complejas. Ingrese nuevos valores de a,b y c.')
     sys.exit()
     
-raiz1 = (-b + math.sqrt(discriminante))/(2*a)
-raiz2 = (-b - math.sqrt(discriminante))/(2*a)
+raiz1 = np.float32((-b + math.sqrt(discriminante))/(2*a))
+raiz2 = np.float32((-b - math.sqrt(discriminante))/(2*a))
 
 print('\nLas raices son:')
 
