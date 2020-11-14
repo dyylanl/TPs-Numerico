b9040409f57f1878bdac0743228479893b0e6d26
gcompanys <51275717+gcompanys@users.noreply.github.com>
2020-11-14 19:42:34 -0300

    .

--- TP 1/Metodos Busqueda de Raices/RaicesRealesDeCuadratica.py
+++ TP 1/Metodos Busqueda de Raices/RaicesRealesDeCuadratica.py
@@ -1,30 +1,31 @@
 import math
 import sys
+import numpy as np
 
 print('\n Ingrese los valores ax^2 + bx +c:')
 
-a = float(input('a: '))
-b = float(input('b: '))
-c = float(input('c: '))
+a = np.float32(input('a: '))
+b = np.float32(input('b: '))
+c = np.float32(input('c: '))
 
 if (a != 1):   
-        b = b/a # hago una reescala de b
-        c = c/a # hago una reescala de c
+        b = np.float32(b/a) # hago una reescala de b
+        c = np.float32(c/a) # hago una reescala de c
         a = 1   # pongo a en 1
     
 # defino mis valores m (punto medio entre las raices)
 # y d (distancia desde m a las raices)
 
-m = (-b)/2
+m = np.float32((-b)/2)
 
 if (m**2 -c < 0):
     print('\n Las raices son complejas. Intente de nuevo.')
     sys.exit()
 
-d = math.sqrt(m**2 - c)
+d = np.float32(math.sqrt(m**2 - c))
 
-raiz1 = m + d
-raiz2 = m - d
+raiz1 = np.float32(m + d)
+raiz2 = np.float32(m - d)
 
 print('\n Las raices son: \n')
 print('Raiz 1:',raiz1,'Raiz 2:',raiz2)
