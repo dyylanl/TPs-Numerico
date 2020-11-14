f01a11877090c1497fbc705ab8db289678b87105
gcompanys <51275717+gcompanys@users.noreply.github.com>
2020-11-14 14:49:38 -0300

    .

--- TP 1/Metodos Busqueda de Raices/RaicesRealesDeCuadraticaConResolvente.py
+++ TP 1/Metodos Busqueda de Raices/RaicesRealesDeCuadraticaConResolvente.py
@@ -1,11 +1,14 @@
 import math
 import sys
+import numpy as np
 
 print('\nIngrese los valores ax^2 + bx +c:')
 
-a = float(input('a: '))
-b = float(input('b: '))
-c = float(input('c: '))
+#el np.int32 es para restringir a 32 bits 
+
+a = np.int32(input("a: "))
+b = np.int32(input('b: '))
+c = np.int32(input('c: '))
 
 discriminante = b**2 - 4*a*c
 
