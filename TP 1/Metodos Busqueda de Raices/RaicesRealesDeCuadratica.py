0c58d95c76b23803ffff34fc980c374bd7493a45
gcompanys <51275717+gcompanys@users.noreply.github.com>
2020-11-17 17:20:32 -0300

    Cambio de lógica del algoritmo

--- TP 1/Metodos Busqueda de Raices/RaicesRealesDeCuadratica.py
+++ TP 1/Metodos Busqueda de Raices/RaicesRealesDeCuadratica.py
@@ -1,31 +1,33 @@
-import math
-import sys
 import numpy as np
+import sys
+
 
 print('\n Ingrese los valores ax^2 + bx +c:')
 
-a = np.float32(input('a: '))
+a = np.float32(input("a: "))
 b = np.float32(input('b: '))
 c = np.float32(input('c: '))
 
-if (a != 1):   
-        b = np.float32(b/a) # hago una reescala de b
-        c = np.float32(c/a) # hago una reescala de c
-        a = 1   # pongo a en 1
-    
-# defino mis valores m (punto medio entre las raices)
-# y d (distancia desde m a las raices)
+discriminante = b**2-4*a*c
 
-m = np.float32((-b)/2)
-
-if (m**2 -c < 0):
-    print('\n Las raices son complejas. Intente de nuevo.')
+if discriminante == 0:
+    print('\nRaiz doble:',np.float32((-b)/2*a))
     sys.exit()
+    
+if discriminante < 0:
+    print('\nLas raices no son reales.')
+    sys.exit()
+    
+if b >= 0:
+    raiz1 = np.float32((-b - np.sqrt(discriminante))/(2*a))
+    raiz2 = np.float32((2*c)/(-b - np.sqrt(discriminante)))
+    
+if b < 0:
+    raiz1 = np.float32((2*c)/(-b + np.sqrt(discriminante)))
+    raiz2 = np.float32((-b + np.sqrt(discriminante))/(2*a))
+    
+print('\nLas raices son:\n')
+print('Raiz 1:',raiz1)
+print('\nRaiz 2:',raiz2)
 
-d = np.float32(math.sqrt(m**2 - c))
 
-raiz1 = np.float32(m + d)
-raiz2 = np.float32(m - d)
-
-print('\n Las raices son: \n')
-print('Raiz 1:',raiz1,'Raiz 2:',raiz2)
