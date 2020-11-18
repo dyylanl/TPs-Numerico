73d86431892a65d8154d8405d4d9e657111e36c1
gcompanys <51275717+gcompanys@users.noreply.github.com>
2020-11-18 14:21:58 -0300

    .

--- TP 1/Metodos Busqueda de Raices/RaicesRealesDeCuadratica.py
+++ TP 1/Metodos Busqueda de Raices/RaicesRealesDeCuadratica.py
@@ -1,14 +1,19 @@
 import numpy as np
 import sys
 
-
 print('\n Ingrese los valores ax^2 + bx +c:')
 
 a = np.float32(input("a: "))
 b = np.float32(input('b: '))
 c = np.float32(input('c: '))
 
-discriminante = b**2-4*a*c
+def sgn(b):
+    if b < 0:
+        return -1
+    else:
+        return 1
+
+discriminante = np.float32(b**2-4*a*c)
 
 if discriminante == 0:
     print('\nRaiz doble:',np.float32((-b)/2*a))
@@ -17,17 +22,15 @@ if discriminante == 0:
 if discriminante < 0:
     print('\nLas raices no son reales.')
     sys.exit()
-    
-if b >= 0:
-    raiz1 = np.float32((-b - np.sqrt(discriminante))/(2*a))
-    raiz2 = np.float32((2*c)/(-b - np.sqrt(discriminante)))
-    
-if b < 0:
-    raiz1 = np.float32((2*c)/(-b + np.sqrt(discriminante)))
-    raiz2 = np.float32((-b + np.sqrt(discriminante))/(2*a))
+
+
+x1 = np.float32(-(b + sgn(b)*np.sqrt(discriminante))/(2*a))
+x2 = np.float32(c/(a*x1))
+
     
 print('\nLas raices son:\n')
-print('Raiz 1:',raiz1)
-print('\nRaiz 2:',raiz2)
+print('Raiz 1:',x1)
+print('\nRaiz 2:',x2)
+
 
 
