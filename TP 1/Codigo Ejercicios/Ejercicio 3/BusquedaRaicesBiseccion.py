06079ed139bd14240fd983e3ea370996e96a8ef2
Antonella Ponce <44786355+antonellaponce28@users.noreply.github.com>
2020-11-18 15:30:19 -0300

    Modifico para que retorne la historia de raices

--- TP 1/Codigo Ejercicios/Ejercicio 3/BusquedaRaicesBiseccion.py
+++ TP 1/Codigo Ejercicios/Ejercicio 3/BusquedaRaicesBiseccion.py
@@ -9,22 +9,26 @@ def busqueda_raiz(inicio, fin, epsilon, func, iteraciones = None):
     N = iteraciones
     if(iteraciones == None):
         N = math.ceil(np.log2((fin-inicio)/epsilon))
-        
+    historia = np.zeros((N, 2))
+    i = 0
     #print("Iteraciones: " + str(N))
-    resultado = busqueda_raiz_rec(inicio, fin, N, func)
+    resultado = busqueda_raiz_rec(inicio, fin, N, func, historia, i)
     resultado[2] = N
     return resultado
     
 
    
-def busqueda_raiz_rec(inicio, fin, N, func):
+def busqueda_raiz_rec(inicio, fin, N, func, historia, i):
     medio = (inicio+fin)/2
     if (func(medio) == 0 or N <= 1):
-        return [medio, (fin-inicio)/2, 0]
+        historia = historia[:i]
+        return [medio, (fin-inicio)/2, 0, historia]
+    
+    historia[i] = (i+1, medio)
+    i = i + 1
     
     if (func(medio) * func(inicio) > 0):
-        return busqueda_raiz_rec(medio, fin, N-1, func)
+        return busqueda_raiz_rec(medio, fin, N-1, func, historia, i)
     
     else:    # (f(medio) * f(inicio) < 0):
-        return busqueda_raiz_rec(inicio, medio, N-1, func)
-
+        return busqueda_raiz_rec(inicio, medio, N-1, func, historia, i)
