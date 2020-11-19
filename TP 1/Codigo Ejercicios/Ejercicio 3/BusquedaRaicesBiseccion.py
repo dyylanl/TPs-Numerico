640ee1f00a3bdac6d20629b5637aa100053c61f3
Antonella Ponce <44786355+antonellaponce28@users.noreply.github.com>
2020-11-19 15:06:39 -0300

    Me guarda ultima iteracion

--- TP 1/Codigo Ejercicios/Ejercicio 3/BusquedaRaicesBiseccion.py
+++ TP 1/Codigo Ejercicios/Ejercicio 3/BusquedaRaicesBiseccion.py
@@ -21,14 +21,17 @@ def busqueda_raiz(inicio, fin, epsilon, func, iteraciones = None):
 def busqueda_raiz_rec(inicio, fin, N, func, historia, i):
     medio = (inicio+fin)/2
     if (func(medio) == 0 or N <= 1):
-        historia = historia[:i]
+        historia[i] = (i, medio)
+        historia = historia[:i+1]
         return [medio, (fin-inicio)/2, 0, historia]
-    
-    historia[i] = (i+1, medio)
-    i = i + 1
+
     
     if (func(medio) * func(inicio) > 0):
+        historia[i] = (i, medio)
+        i = i + 1
         return busqueda_raiz_rec(medio, fin, N-1, func, historia, i)
     
     else:    # (f(medio) * f(inicio) < 0):
+        historia[i] = (i, medio)
+        i = i + 1
         return busqueda_raiz_rec(inicio, medio, N-1, func, historia, i)
