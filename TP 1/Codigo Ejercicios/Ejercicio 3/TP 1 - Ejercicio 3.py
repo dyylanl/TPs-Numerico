8f41f624c5e73882dc0c055dfe6f38b88f72e235
camiSebe <csebellin@fiuba.ar>
2020-11-19 12:49:25 -0300

    cambios a la llamada a la funcion busuqedaRaicesPuntoFijo

--- TP 1/Codigo Ejercicios/Ejercicio 3/TP 1 - Ejercicio 3.py
+++ TP 1/Codigo Ejercicios/Ejercicio 3/TP 1 - Ejercicio 3.py
@@ -6,7 +6,7 @@ import sympy as sym
 import BusquedaRaicesBiseccion as biseccion
 import BusquedaRaicesNewton as newton
 import BusquedaRaicesMultiplesNewton as newtonMult
-#import BusquedaRaicesPuntoFijo as puntoFijo
+import BusquedaRaicesPuntoFijo as puntoFijo
 import BusquedaRaicesSecante as secante
 
 import OrdenDeConvergencia as ordenConvegencia
@@ -36,87 +36,93 @@ def volumenLlenoAPorcentaje(porcentaje):
 
 
 def graficarFuncionesYDerivadas(mostrarLeyenda = False):
-    
+
     funcionVolumenPorcentajeEnSimbolos = sym.simplify((math.pi * (x ** 2)* (3*R-x))/3)
     funcionVolumenPorcentajeDerivada = sym.simplify(sym.diff(funcionVolumenPorcentajeEnSimbolos,x))
-    
+
     graficoVolumen = sym.plotting.plot(funcionVolumenPorcentajeEnSimbolos, (x, 0, 2*R), title = "Grafico Volumen y Derivada segun el Radio del Tanque", label = "Funcion Volumen", xlabel = "Radio", ylabel = "Volumen", legend = mostrarLeyenda)
     graficoDerivada = sym.plotting.plot(funcionVolumenPorcentajeDerivada, (x, 0, 2*R),  line_color = 'r', label = "Derivada de Volumen segun el Radio", legend = mostrarLeyenda)
     graficoVolumen.extend(graficoDerivada)
     graficoVolumen.extend(sym.plotting.plot(volumenDelTanqueDeAgua(2*R), (x, 0, 2*R), line_color = 'g', label = "Volumen maximo", legend = mostrarLeyenda))
     graficoVolumen.extend(sym.plotting.plot(0, (x, 0, 2*R), line_color = "Yellow", label = "Derivada Volumen Maximo", legend = mostrarLeyenda))
-    
-    
+
+
     graficoVolumen.show()
 
 
 def funcionParaNewton(x):
     return
-    
-    
+
+
 def funcionParaNewtonMult(x):
     return
-    
-        
-def funcionParaPuntoFijo(x):
-    return
-    
-    
+
 def funcionParaBiseccion(x):
     funcionAltura = sym.simplify((-1* (h**3)) + 3*R*(h**2) - (3 * volumenAHallar/ math.pi))
     return funcionAltura.subs(h,x)
 
+def funcionFParaPuntoFijo(x):
+    funcionAltura = sym.simplify((-1* (h**3)) + 3*R*(h**2) - (3 * volumenAHallar/ math.pi))
+    return (funcionAltura.subs(h,x))
+
+def funcionGParaPuntoFijo(x):
+    GdeXParaPuntoFijo = sym.simplify( ((3*R*(h**2)) - (3 * volumenAHallar/ math.pi ))**(1/3))
+    return(GdeXParaPuntoFijo.subs(h,x))
 
 def buscarRaicesConDistintosMetodosYCota(cota):
-    
-    
+
+
     print("\n Usando la cota de error: " + str(cota)+ "\n ")
-    
+
     print("===========================================" + "\n")
-    print ("\t \t \t BISECCION \n")    
+    print ("\t \t \t BISECCION \n")
     print("===========================================" + "\n")
 
     resultadoBiseccion = biseccion.busqueda_raiz(0, 2*R,cota, funcionParaBiseccion)
     print("El resultado por Bisección es: "+ str(resultadoBiseccion[:3]) + "\n")
-    
-    #resultadoPuntoFijo = puntoFijo.busqueda_raiz(0, 2*R, cota, funcionParaPuntoFijo)
-    #print("El resultado por Punto Fijo es: "+ str(resultadoPuntoFijo))
-    
+
     x= sym.Symbol('x')
     y= sym.Symbol('y')
+    semillaPuntoFijo = biseccion.busqueda_raiz(0,0, 2*R,cota, funcionParaBiseccion, 3)[0]
     semillaNewton = biseccion.busqueda_raiz(0, 2*R,cota, funcionParaBiseccion, 5)[0]
     semillaAux = biseccion.busqueda_raiz(0, 2*R,cota, funcionParaBiseccion, 4)[0] # Solo para secante
-    
- 
-    print ("--------------- SEMILLAS ----------------- \n")    
 
+
+    print ("--------------- SEMILLAS ----------------- \n")
+
+    print("Semilla elegida con 3 iteraciones de biseccion: "+ str(semillaPuntoFijo))
     print("Semilla elegida con 5 iteraciones de biseccion: "+ str(semillaNewton))
     print("Semilla auxiliar elegida con 4 iteraciones de biseccion: "+ str(semillaAux) + "\n")
-    
-    
+
+
+
+    resultadoPuntoFijo = puntoFijo.busqueda_raiz_punto_fijo(semillaPuntoFijo, cota, funcionFParaPuntoFijo,funcionGParaPuntoFijo)
+    #print("El resultado por Punto Fijo es: "+ str(resultadoPuntoFijo))
+
+
     funcionNewton = sym.simplify((-1* (x**3)) + 3*R*(x**2) - (3 * volumenAHallar/ math.pi))
-    
+
     resultadoSecante = secante.busqueda_raiz_secante(funcionNewton, semillaNewton, semillaAux,  cota)
     print("===========================================" + "\n")
-    print ("\t \t \t SECANTE \n")    
-    print("===========================================" + "\n")  
+    print ("\t \t \t SECANTE \n")
+    print("===========================================" + "\n")
     print("El resultado por Secante es: "+ str(resultadoSecante) + "\n")
-    
+
     resultadoNewton = newton.busqueda_raiz_newton(funcionNewton, semillaNewton, cota)
     print("===========================================" + "\n")
-    print ("\t \t \t NEWTON \n")    
-    print("===========================================" + "\n")    
+    print ("\t \t \t NEWTON \n")
+    print("===========================================" + "\n")
     print("El resultado por Newton es: "+ str(resultadoNewton[:3]) + "\n")
-     
+
     resultadoNewtonMult = newtonMult.busqueda_raiz_newton(funcionNewton, semillaNewton, cota)
     print("===========================================" + "\n")
-    print ("\t \t \t NEWTON MULTIPLE \n")    
-    print("===========================================" + "\n")   
+    print ("\t \t \t NEWTON MULTIPLE \n")
+    print("===========================================" + "\n")
     print("El resultado por Newton Mult es: "+ str(resultadoNewtonMult[:3]))
-    
+
 x= sym.Symbol('x')
 
-h= sym.Symbol('h') 
+h= sym.Symbol('h')
 
 graficarFuncionesYDerivadas()
 
@@ -134,6 +140,3 @@ print("a) Volumen del tanque lleno al "+ str(porcentajePedido*100) +  "% calcula
 print("b) El volumen total del tanque es: " + str(volumenLlenoAPorcentaje(1)))
 
 buscarRaicesConDistintosMetodosYCota(10**(-5))
-
-
-
